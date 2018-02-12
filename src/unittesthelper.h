e2e4133cd5a28456bf2021e610e78fab7db556f8
zhengzhiyong <zhengzhiyong@sogou-inc.com>
2018-02-12 17:19:53 +0800

    unittesthelper supports the use of ENV to pass parameters

--- src/unittesthelper.h
+++ src/unittesthelper.h
@@ -1,5 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <map>
 
 #define COLOR_RED     "\x1b[31m"
 #define COLOR_GREEN   "\x1b[32m"
@@ -15,21 +17,44 @@ struct UNITTEST_HELPER {
 
 #define SAFE_DELETE(ptr) do { delete ptr; (ptr) = 0; } while (0)
 
-#define check(r, fmt, arg...) do {                                                  \
-  if ((r)) break;                                                                   \
-  fprintf(stderr, "%-5d %s -> ["COLOR_RED fmt COLOR_RESET"]\n", __LINE__, #r, ##arg); \
-  exit(1);                                                                          \
+#define CHECK_IMPL(r, name, fmt, arg...) do {                      \
+  if ((r)) break;                                                  \
+  fprintf(stderr, "%s@%-5d %s -> ["COLOR_RED fmt COLOR_RESET"]\n", \
+          name, __LINE__, #r, ##arg);                              \
+  exit(1);                                                         \
 } while(0)
 
-#define DEFINE(name)  struct TEST_##name {};                        \
-  template<> struct UNITTEST_HELPER<TEST_##name> { void call(); };  \
-  void UNITTEST_HELPER<TEST_##name>::call()
+#define check(r, fmt, arg...)  CHECK_IMPL(r, __FILE__, fmt, ##arg)
+#define checkx(r, fmt, arg...) CHECK_IMPL(r, TEST_NAME_, fmt, ##arg)
 
-#define TEST_IMPL(name, t) do {                                        \
-  UNITTEST_HELPER<TEST_##name> test_##name;                            \
-  test_##name.call();                                                  \
-  if (t) printf("TEST %-20s ["COLOR_GREEN"OK"COLOR_RESET"]\n", #name); \
+inline
+void *env_safe_get(const std::map<std::string, void *> &map,
+                   const std::string &key, void *def) {
+  std::map<std::string, void *>::const_iterator pos = map.find(key);
+  if (pos == map.end()) {
+    if (def) return def;
+    fprintf(stderr, COLOR_RED "ENV %s notfound" COLOR_RESET, key.c_str());
+    exit(1);
+  } else {
+    return pos->second;
+  }
+}
+
+#define UNITTEST_INIT() static std::map<std::string, void *> UNITTEST_ENV
+#define ENV_SET(key, value) UNITTEST_ENV[(key)] = (value)
+#define ENV_GET(key, type) (type) (env_safe_get(UNITTEST_ENV, (key), 0))
+
+#define DEFINE(func)  struct TEST_##func {};                        \
+  template<> struct UNITTEST_HELPER<TEST_##func> {                  \
+    UNITTEST_HELPER(const char *name) : TEST_NAME_(name) {}         \
+    void call(); const char *TEST_NAME_; };                         \
+  void UNITTEST_HELPER<TEST_##func>::call()
+
+#define TEST_IMPL(func, name, t) do {                                  \
+  UNITTEST_HELPER<TEST_##func> test_##func(name); test_##func.call();  \
+  if (t) printf("TEST %-60s ["COLOR_GREEN"OK"COLOR_RESET"]\n", name); \
 } while(0)
 
-#define DO(name) TEST_IMPL(name, false)
-#define TEST(name) TEST_IMPL(name, true)
+#define DO(func)          TEST_IMPL(func, #func, false)
+#define TEST(func)        TEST_IMPL(func, #func, true)
+#define TESTX(func, name) TEST_IMPL(func, name, true)
